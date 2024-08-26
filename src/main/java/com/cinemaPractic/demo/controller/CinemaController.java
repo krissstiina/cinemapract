e29e990dc417f33495c1f33991cee98e43994704
krissstiina <162792967+krissstiina@users.noreply.github.com>
2024-08-26 18:33:46 +0300

    controller

--- src/main/java/com/cinemaPractic/demo/controller/CinemaController.java
+++ src/main/java/com/cinemaPractic/demo/controller/CinemaController.java
@@ -1,8 +1,5 @@
 package com.cinemaPractic.demo.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cinemaPractic.demo.model.CinemaDTO;
 import com.cinemaPractic.demo.model.CreateCinemaDTO;
 import com.cinemaPractic.demo.model.UpdateCinemaDTO;
+import com.cinemaPractic.demo.repositories.CinemaRepository;
 import com.cinemaPractic.demo.service.CinemaService;
 
 @RestController
@@ -23,25 +21,20 @@ public class CinemaController {
     @Autowired
     private CinemaService cinemaService;
 
-    @GetMapping("/{id}")
-    public CinemaDTO getById(@PathVariable int id) {
-        Optional<CinemaDTO> cinemaDTO = cinemaService.findById(id);
-        return cinemaDTO.get();
-    }
-
-    @GetMapping("")
-    public List<CinemaDTO> getAll(){
-        return cinemaService.findAll();
-    }
-
     @PostMapping("")
     public CinemaDTO create(@RequestBody CreateCinemaDTO createCinemaDTO){
         return cinemaService.create(createCinemaDTO);
     }
 
     @PatchMapping("")
-    public CinemaDTO update(@RequestBody UpdateCinemaDTO updateCinemaDTO){
-        return cinemaService.update(updateCinemaDTO);
+    public CinemaDTO update(@RequestBody UpdateCinemaDTO cinemaUpdateDto){
+        return cinemaService.update(cinemaUpdateDto);
     }
+
+    @GetMapping("/{id}")
+    public CinemaDTO getById(@PathVariable int id){
+        return cinemaService.findById(id);
+    }
+
 
 }
